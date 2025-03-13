29a08e18e648a52980890c53da45d36ce684493e
Saulo Barros <saulobarros1997@gmail.com>
2025-03-13 15:53:52 -0300

    fix: corrigindo identação do arquivo de configurações

--- cypress.config.js
+++ cypress.config.js
@@ -3,36 +3,36 @@ const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 async function setupNodeEvents(on, config) {
-    // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-    await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
 
-    on("file:preprocessor", browserify.default(config));
+  on("file:preprocessor", browserify.default(config));
 
-    // Make sure to return the config object as it might have been modified by the plugin.
-    return config;
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
 }
 
 module.exports = defineConfig({
 
-    viewportWidth: 1920,
-    viewportHeight: 1080,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
 
-    e2e: {
-        specPattern: "**/**/*.feature",
-        supportFile: 'cypress/support/index.js',
-        fixture:'cypress/fixtures/*.json',
-        "baseUrl": "https://demoqa.com",
-        env: {
-          
-        },
-        chromeWebSecurity: false,
-        modifyObstructiveCode:false,
-        "video":true,
-        "videoCompression": 0,
-        "animationDistanceThreshold": 50,
-        "pageLoadTimeout": 120000,
-        "defaultCommandTimeout": 60000,
-        "requestTimeout": 30000,
-        setupNodeEvents,
+  e2e: {
+    env: {
+        
     },
+    specPattern: "**/**/*.feature",
+    supportFile: 'cypress/support/index.js',
+    fixture:'cypress/fixtures/*.json',
+    "baseUrl": "http://localhost:8080",
+    chromeWebSecurity: false,
+    modifyObstructiveCode:false,
+    "video":true,
+    "videoCompression": 0,
+    "animationDistanceThreshold": 50,
+    "pageLoadTimeout": 120000,
+    "defaultCommandTimeout": 60000,
+    "requestTimeout": 30000,
+    setupNodeEvents,
+  },
 });
